99aab505cec5dd6cd26da549341813f8fdb59b92
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-07-05 21:14:25 -0400

    Expose models through index

--- service/app/models/index.js
+++ service/app/models/index.js
@@ -1,7 +1,15 @@
+import ChallengeModel from './challenges.js';
+import DashOffModel from './dashoff.js';
+import ExtraModel from './extras.js';
 import User from './user.js';
+import ScoresModel from './scores.js'
 
 
 
 export default {
   User,
+  Extra: ExtraModel,
+  DashOff: DashOffModel,
+  Challenge: ChallengeModel,
+  Score: ScoresModel
 };
