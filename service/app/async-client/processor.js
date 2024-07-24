1eb4f88c641da169532b3e6bfd78ad0499a9d8de
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-07-24 07:28:41 -0400

    Update import

--- service/app/async-client/processor.js
+++ service/app/async-client/processor.js
@@ -1,5 +1,5 @@
-import getClient from "./client";
-import { ASYNC_JOB_HOST, ASYNC_JOB_PORT } from "../../config";
+import getClient from "./client.js";
+import { ASYNC_JOB_HOST, ASYNC_JOB_PORT } from "../../config.js";
 export const evaluateDashOff = async (dashOffId, rawText) => {
   let client = getClient(ASYNC_JOB_HOST, ASYNC_JOB_PORT)
 
