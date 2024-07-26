8803083a21319dcb11acb769245bb1854eafaf01
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-07-26 06:56:23 -0400

    Fix mispell

--- service/app/async-client/processor.js
+++ service/app/async-client/processor.js
@@ -4,7 +4,7 @@ export const evaluateDashOff = async (dashOffId, rawText) => {
   let client = getClient(ASYNC_JOB_HOST, ASYNC_JOB_PORT)
 
   await client.connect()
-  client,getResult(dashOffId, rawText).then(data => {
+  client.getResult(dashOffId, rawText).then(data => {
     console.log(data)
   }).catch(e => {console.log(e)})
   client,disconnect()
