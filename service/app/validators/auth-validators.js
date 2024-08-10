96d1feddd74851592351e0353116e4d8e9ce21ee
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-08-10 09:39:12 -0400

    Fix: Allow special chars in password

--- service/app/validators/auth-validators.js
+++ service/app/validators/auth-validators.js
@@ -6,8 +6,8 @@ export const registerUserSchema = yup.object({
   password: yup.string().required("Please enter password !")
         .min(8, 'Password is too short - should be 8 chars minimum.')
         .matches(
-          /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-          'Minimum eight characters, at least one letter and one number'
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/,
+          'Minimum eight characters, at least one alphabet, one special char and one number'
         ),
   passwordConfirmation: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required(),
   email: yup.string().email().required(),
