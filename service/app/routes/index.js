cc81a0f370eca3c3c9848b426583d8983c4c7dac
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-08-10 20:46:07 -0400

    Add status endpoint

--- service/app/routes/index.js
+++ service/app/routes/index.js
@@ -2,12 +2,31 @@ import basicRouter from './basic-route.js';
 import authRouter from './auth-routes.js';
 import adminRouter from './admin-routes.js';
 import userRouter from './user-routes.js';
+import axios from 'axios';
 
 const initalizeRoutes = (app) => {
   app.use("/api/v1/auth/", authRouter);
   app.use("/api/v1/admin/", adminRouter);
   app.use('/api/v1/eval/', basicRouter);
   app.use('/api/v1/', userRouter);
+  app.get('/status', async (req, res) => {
+    try {
+    await axios.get(process.env.SCHEDULER);
+    res.json({
+      status: 'OK',
+      message: 'Server is running',
+      timestamp: new Date().toISOString()
+    });
+  } catch {
+    console.error("Status call failed...")
+    res.json({
+      status: 'OK',
+      message: 'Server is running',
+      timestamp: new Date().toISOString(),
+      error: true,
+    });
+  }
+  });
 }
 
 export default initalizeRoutes;
